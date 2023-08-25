9eb389be7707ff32b18d467897ae7b57133b8793
Amirali Tavakoli <amiralitavakoli80@gmail.com>
2023-08-25 17:17:44 +0330

    add a temporary thread pool write pipeline API and its implementation

--- include/pipeline.h
+++ include/pipeline.h
@@ -1,3 +1,50 @@
 ﻿#pragma once
 
-#include <iostream>
+#include <future>
+#include <vector>
+
+namespace BS
+{
+	class thread_pool;
+}
+
+namespace pip
+{
+	class PipelineElement
+	{
+	public:
+		virtual ~PipelineElement() = default;
+	};
+
+	using PipeFunction = PipelineElement * (*)(PipelineElement*);
+	using PipeElementGenerator = PipelineElement * (*)();
+	using PipeElementDeleter = void (*)(const PipelineElement*);
+
+	class Pipeline
+	{
+		BS::thread_pool* m_threadPool;
+		std::future<void> m_pipelineLoopFuture;
+		bool m_isPipelineStopped;
+
+		PipeElementGenerator m_elementGenerator;
+		PipeElementDeleter m_elementDeleter;
+		std::vector<PipeFunction> m_pipes;
+
+		void pipelineLoop() const;
+
+	public:
+		Pipeline(const Pipeline& _other) = delete;
+		Pipeline(Pipeline&& _other) noexcept = delete;
+		Pipeline& operator=(const Pipeline& _other) = delete;
+		Pipeline& operator=(Pipeline&& _other) noexcept = delete;
+
+		Pipeline(unsigned int _pipeCount, PipeElementGenerator _elementGenerator);
+		Pipeline(unsigned int _pipeCount, PipeElementGenerator _elementGenerator, PipeElementDeleter _elementDeleter);
+		~Pipeline();
+
+		void AddPipe(PipeFunction _newPipe);
+		void Run();
+		void Stop();
+	};
+	
+}
--- src/pipeline.cpp
+++ src/pipeline.cpp
@@ -1,9 +1,79 @@
-﻿#include "pipeline.h"
+﻿#include "Pipeline.h"
 
-#include <iostream>
+#include "thread_pool.h"
 
-int main()
+using namespace pip;
+
+Pipeline::Pipeline(const unsigned int _pipeCount, const PipeElementGenerator _elementGenerator)
+	: m_threadPool(new BS::thread_pool(_pipeCount))
+	, m_isPipelineStopped(true)
+	, m_elementGenerator(_elementGenerator)
+	, m_elementDeleter([](const PipelineElement* _elem) { delete _elem; })
 {
-	std::cout << "Hello World." << std::endl;
-	return 0;
+	m_pipes.reserve(_pipeCount - 1);
+}
+
+Pipeline::Pipeline(const unsigned int _pipeCount, const PipeElementGenerator _elementGenerator, const PipeElementDeleter _elementDeleter)
+	: Pipeline(_pipeCount, _elementGenerator)
+{
+	m_elementDeleter = _elementDeleter;
+}
+
+Pipeline::~Pipeline()
+{
+	if (m_pipelineLoopFuture.valid())
+		m_pipelineLoopFuture.get();
+	delete m_threadPool;
+}
+
+void Pipeline::AddPipe(const PipeFunction _newPipe)
+{
+	if (!m_isPipelineStopped)
+		Stop();
+	m_pipes.push_back(_newPipe);
+}
+
+void Pipeline::Stop()
+{
+	m_isPipelineStopped = true;
+	if (!m_pipelineLoopFuture.valid())
+		throw std::runtime_error("Pipeline is not running");
+	
+	m_pipelineLoopFuture.get();
+}
+
+void Pipeline::Run()
+{
+	if (m_pipelineLoopFuture.valid() && m_pipelineLoopFuture.wait_for(std::chrono::seconds(0)) != std::future_status::ready)
+		throw std::runtime_error("Pipeline is already running");
+	m_isPipelineStopped = false;
+	m_pipelineLoopFuture = std::async(std::launch::async, [this] { pipelineLoop(); });
+}
+
+void Pipeline::pipelineLoop() const
+{
+	std::vector<PipelineElement*> pipelineElements(m_pipes.size() + 1, nullptr);
+
+	while (!m_isPipelineStopped)
+	{
+		std::vector<std::future<PipelineElement*>> pipeFutures;
+		pipeFutures.reserve(m_pipes.size() + 1);
+
+		std::future<PipelineElement*> firstPipeFuture =
+			m_threadPool->submit([this]() { return m_elementGenerator(); });
+		pipeFutures.push_back(std::move(firstPipeFuture));
+
+		for (size_t idx = 0 ; idx < pipelineElements.size() - 1 ; ++idx)
+		{
+			std::future<PipelineElement*> pipeFuture =
+				m_threadPool->submit([&, idx]() { return m_pipes[idx](pipelineElements[idx + 1]); });
+			pipeFutures.push_back(std::move(pipeFuture));
+		}
+
+		m_elementDeleter(pipeFutures[pipeFutures.size() - 1].get());
+		for (size_t idx = pipelineElements.size() - 1; idx > 0; --idx)
+		{
+			pipelineElements[idx] = pipeFutures[idx - 1].get();
+		}
+	}
 }
